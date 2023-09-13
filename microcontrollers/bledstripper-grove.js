3c797fdd79b22bded491ba5f9fb50f4754f17950
jeffyactive <jeff@reelyactive.com>
2023-09-13 11:13:36 -0400

    Completed initial implementation of command parsing

--- microcontrollers/bledstripper-grove.js
+++ microcontrollers/bledstripper-grove.js
@@ -3,16 +3,71 @@
  * We believe in an open Internet of Things
  */
 
+const COMMAND_CLEAR_STRIP = 0x00;
+const COMMAND_WRITE_STRIP = 0x01;
+const EXPECTED_STRIP_LENGTH = 256;
+
+const STRIP_PINS = [ D12, D13, D14, D15 ];
+
 let greenLedOn = false;
 
-function handleStripData(evt) {
-  let pin = D12;
+function handleClearStrip(message) {
+  if((message.byteLength !== 2) || (message[1] > STRIP_PINS.length)) {
+    return blipErrorLed();
+  }
 
-  require("neopixel").write(pin, []);
+  let pin = STRIP_PINS[message[1]];
+  let rgbArray = new Uint8ClampedArray(EXPECTED_STRIP_LENGTH * 3).fill(0);
+  require("neopixel").write(pin, rgbArray);
+}
+
+function handleWriteStrip(message) {
+  if((message.byteLength !== 12) || (message[1] > STRIP_PINS.length)) {
+    return blipErrorLed();
+  }
+
+  let pin = STRIP_PINS[message[1]];
+  let startLedIndex = (message[2] * 256) + message[3];
+  let endLedIndex = (message[4] * 256) + message[5];
+
+  if(endLedIndex < startLedIndex) {
+    return blipErrorLed();
+  }
+
+  let rgbArray = new Uint8ClampedArray(EXPECTED_STRIP_LENGTH * 3).fill(0);
+
+  for(let ledIndex = startLedIndex; ledIndex < endLedIndex; ledIndex++) {
+    rgbArray[ledIndex * 3] = message[6];       // Red
+    rgbArray[(ledIndex * 3) + 1] = message[7]; // Green
+    rgbArray[(ledIndex * 3) + 2] = message[8]; // Blue
+  }
+  require("neopixel").write(pin, rgbArray);
+}
+
+function handleStripData(evt) {
+  let message = new Uint8ClampedArray(evt.data);
+
+  if(message.byteLength < 1) {
+    return blipErrorLed();
+  }
+
+  switch(message[0]) {
+    case COMMAND_CLEAR_STRIP:
+      return handleClearStrip(message);
+    case COMMAND_WRITE_STRIP:
+      return handleWriteStrip(message);
+    default:
+      return blipErrorLed();
+  }
 }
 
 function toggleGreenLed() {
   D5.write(greenLedOn = !greenLedOn);
+}
+
+function blipErrorLed() {
+  D3.set();
+  setTimeout(function() { D3.reset(); }, 500);
 }
 
 NRF.setServices({
@@ -26,8 +81,8 @@ NRF.setServices({
 });
 
 // Blue LED indicates connection status
-NRF.on('connect', function(addr) { D4.write(true); });
-NRF.on('disconnect', function(addr) { D4.write(false); });
+NRF.on('connect', function(addr) { D4.set(); });
+NRF.on('disconnect', function(addr) { D4.reset(); });
 
 // Green LED indicates operation
 setInterval(toggleGreenLed, 1000);
